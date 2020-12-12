ed5781ff18dd3f942e9ff89e4546d2186d9e0556
MarcosRodrigues <marcosrodriguessc@hotmail.com>
2020-12-12 13:50:53 -0300

    'pagina'

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,10 @@
 import { useState } from 'react';
 
 function Home() {
-    return (
+    return
         <div>
             <h1>Homeee</h1>
-            <Contador />
         </div>
-    )
 
-}
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function adicionarContador() {
-        setContador(contador + 1);
-    }
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    )
 }
 export default Home
